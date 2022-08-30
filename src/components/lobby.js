cd9c1cf8e96ceb2cf25aeae2056a3d5fb12806b5
itzVort3x1 <saikaustubh10@gmail.com>
2022-08-30 18:09:53 +0530

    added components

---
+++ src/components/lobby.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LobbyPage = () => {
+     return (
+          <div>
+               <h1>Dashboard</h1>
+          </div>
+     );
+};
+
+export default LobbyPage;
