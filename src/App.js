6091d9ee620ab93fcb1ca0583f13be1e4d757ef2
itzVort3x1 <saikaustubh10@gmail.com>
2022-08-30 18:14:26 +0530

    finished routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LobbyPage from './components/lobby';
+import DashboardPage from './components/dashboard';
+
+//^5.3.3 use this version of react router dom
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={LobbyPage} />
+          <Route exact path="/dashboard" component={DashboardPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/lobby.js
+++ src/components/lobby.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LobbyPage = () => {
      return (
           <div>
-               <h1>Dashboard</h1>
+               <h1>Lobby</h1>
           </div>
      );
 };
